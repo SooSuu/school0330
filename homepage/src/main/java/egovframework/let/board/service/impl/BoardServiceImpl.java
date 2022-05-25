1e8520b61ea86e655b5802894683ebe6216b5753
Soo <june183@naver.com>
2022-05-25 16:18:14 +0900

    20220525

---
+++ homepage/src/main/java/egovframework/let/board/service/impl/BoardServiceImpl.java
@@ -0,0 +1,51 @@
+package egovframework.let.board.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import egovframework.com.cmm.service.EgovFileMngService;
+import egovframework.com.cmm.service.FileVO;
+import egovframework.let.board.service.BoardService;
+import egovframework.let.board.service.BoardVO;
+import egovframework.let.utl.fcc.service.EgovDateUtil;
+
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+@Service("boardService")
+public class BoardServiceImpl extends EgovAbstractServiceImpl implements BoardService{
+		
+	@Resource(name = "boardMapper")
+	private BoardMapper boardMapper;
+	
+	//idgen--> context-idgen
+	@Resource(name = "egovBoardIdGnrService")
+	private EgovIdGnrService idgenService;
+	
+	//게시물 목록 가져오기
+	public List<EgovMap> selectBoardList(BoardVO vo) throws Exception{
+		return boardMapper.selectBoardList(vo);
+	}
+	//게시물 목록 수
+	public int selectBoardListCnt(BoardVO vo) throws Exception{
+		return boardMapper.selectBoardListCnt(vo);
+	}
+	//게시물 등록하기
+	@Override
+	public String insertBoard(BoardVO vo) throws Exception{
+		String id = idgenService.getNextStringId();
+		vo.setBoardId(id);
+		boardMapper.insertBoard(vo);
+		
+		return id;
+	}
+}
---
+++ homepage/src/main/java/egovframework/let/board/wep/BoardController.java
@@ -0,0 +1,127 @@
+package egovframework.let.board.wep;
+import java.util.List;
+import java.util.Map;
+
+import egovframework.com.cmm.ComDefaultCodeVO;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.service.EgovCmmUseService;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.let.board.service.BoardService;
+import egovframework.let.board.service.BoardVO;
+import egovframework.let.utl.fcc.service.EgovStringUtil;
+
+import egovframework.rte.fdl.cmmn.exception.EgovBizException;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springmodules.validation.commons.DefaultBeanValidator;
+
+@Controller
+public class BoardController {
+	
+	@Resource(name = "boardService")
+	private BoardService boardService;
+	
+	//BOARD 목록 가져오기
+	@RequestMapping(value = "/board/selectList.do")
+	public String selectList(@ModelAttribute("searchVO") BoardVO searchVO,HttpServletRequest request, ModelMap model) throws Exception{
+		
+		//공지 게시글
+		searchVO.setNoticeAt("Y");
+		List<EgovMap> noticeResultList = boardService.selectBoardList(searchVO);
+		model.addAttribute("noticeResultList", noticeResultList);
+		
+		PaginationInfo paginationInfo = new PaginationInfo();
+		
+		paginationInfo.setCurrentPageNo(searchVO.getPageIndex());
+		paginationInfo.setRecordCountPerPage(searchVO.getPageUnit());
+		paginationInfo.setPageSize(searchVO.getPageSize());
+		
+		searchVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+		searchVO.setLastIndex(paginationInfo.getLastRecordIndex());
+		searchVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+		
+		
+		searchVO.setNoticeAt("N");
+		List<EgovMap> resultList = boardService.selectBoardList(searchVO);
+		model.addAttribute("resultList", resultList);	
+		
+		
+		int totCnt = boardService.selectBoardListCnt(searchVO);
+		
+		paginationInfo.setTotalRecordCount(totCnt);
+		model.addAttribute("paginationInfo",paginationInfo);
+		
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		model.addAttribute("USER_INFO", user);
+		
+		return "board/BoardSelectList";
+	}
+	
+	//게시물 등록, 수정
+	@RequestMapping(value = "/board/boardRegist.do")
+	public String boardRegist(@ModelAttribute("searchVO") BoardVO BoardVO,HttpServletRequest request, ModelMap model) throws Exception{
+		
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		if(user == null || user.getId() == null) {
+			model.addAttribute("message", "로그인 후 사용가능합니다.");
+			return "forward:/board/selectList.do";
+		}else {
+			model.addAttribute("USER_INFO", user);
+		}
+		
+		BoardVO result = new BoardVO();
+		if(!EgovStringUtil.isEmpty(BoardVO.getBoardId())) {
+			/*
+			result = boardService.selectBoard(BoardVO);
+			//본인 및 관리자만 허용 (비밀글)
+			if(!user.getId().equals(result.getFrstRegistId()) && !"admin".equals(user.getId())) {
+				model.addAttribute("message", "작성자 본인만 확인 가능합니다.");
+				return "forward:/board/selectList.do";
+			}
+			*/
+		}
+		model.addAttribute("result", result);
+		
+		request.getSession().removeAttribute("sessionBoard");
+		
+		return "board/BoardRegist";
+	}
+	//게시글 등록
+	@RequestMapping(value = "/board/insert.do")
+	public String insert(@ModelAttribute("searchVO") BoardVO searchVO,HttpServletRequest request, ModelMap model) throws Exception{
+	
+		//이중 서브밋 방지 체크
+		if(request.getSession().getAttribute("sessionBoard") != null) {
+			return "forward:/board/selectList.do";
+		}
+		
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		if(user == null || user.getId() == null) {
+			model.addAttribute("message", "로그인 후 사용가능합니다.");
+			return "forward:/board/selectList.do";
+		}
+		
+		searchVO.setCreatIp(request.getRemoteAddr());
+		searchVO.setUserId(user.getId());
+		
+		boardService.insertBoard(searchVO);
+		
+		//이중 서브밋 방지
+		request.getSession().setAttribute("sessionBoard", searchVO);
+		return "forward:/board/selectList.do";
+	}
+}
